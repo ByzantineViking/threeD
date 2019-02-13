40fd1a6ae748eab99b8708740305cf2e56345a92
Teemu Saravirta <teemu.saravirta@aalto.fi>
2019-02-13 17:08:46 +0200

    added a Vector3D class

---
+++ src/threeD/Vector3D.scala
@@ -0,0 +1,18 @@
+package threeD;
+
+
+class Vector3D(val x: Int,val y: Int, val z: Int) {
+  
+  
+  
+  
+  
+  
+  override def toString() = {
+    x.toString +  "," + y.toString + "," + z.toString
+  }
+}
+
+object Vector3D {
+  
+}
